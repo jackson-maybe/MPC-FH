125596bee8754bf73be3e548a67ddd2e04504c87
jackson-maybe <70527184+jackson-maybe@users.noreply.github.com>
2023-04-21 11:01:54 +0800

    Update protocol_compile.sh

--- MP-SPDZ/protocol_compile.sh
+++ MP-SPDZ/protocol_compile.sh
@@ -2,6 +2,7 @@
 make -j8 mpir&&
 make Fake-Offline.x highgear-party.x&&
 sudo make -j 8 online&&
+./Fake-Offline.x 2 -lgp 128&&
 ./Fake-Offline.x 3 -lgp 128&&
 ./Fake-Offline.x 4 -lgp 128&&
 ./Fake-Offline.x 5 -lgp 128&&
